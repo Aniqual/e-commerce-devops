f67e28ba3b4d41eb9f13526075167ca43177eaee
Anitej Sivapurapu <anitej8@gmail.com>
2026-04-16 11:29:20 +0530

    test update

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 DevOps App Running on EC2!");
+  res.send("🚀 DevOps App Running successfully on EC2!");
 });
 
 app.listen(3000, () => {
